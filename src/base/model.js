45e583865d37f5737bb2b27154931fadb7b0240a
amorgunov <aleksandr.morgunov@noveogroup.com>
2016-07-28 16:14:34 +0700

    fix spaces

--- src/base/model.js
+++ src/base/model.js
@@ -6,92 +6,92 @@ export default class Model {
     this.fieldMap = ['id', 'title', 'description', 'likes', 'is_liked'];
   }
 
-	/**
-	 * Save a new item if data is valid.
-	 * @param {object} data
-	 * @param {function} callback
-	 */
-	save(data, callback) {
+  /**
+   * Save a new item if data is valid.
+   * @param {object} data
+   * @param {function} callback
+   */
+  save(data, callback) {
     callback = callback || function () {};
 
-		if (this.validate(data)) {
-			let fields = this.getData(data);
-			this.storage.save(fields, callback);
-		}
-	}
+    if (this.validate(data)) {
+      let fields = this.getData(data);
+      this.storage.save(fields, callback);
+    }
+  }
 
-	/**
-	 * Finds a model in storage. If query is number or sting, then its will be id
-	 * and find model with this id. If query is object, find appropriate models
-	 *
-	 * @param {string|number|object} [query] A query to match models against
-	 * @param {function} [callback] The callback to call after the model(s) is(are) found
-	 *
-	 * @example
-	 * model.find(1, function(){}); // Will find the model with an ID of 1
-	 * model.find({ foo: 'bar' });
-	 */
-	find(query, callback) {
+  /**
+   * Finds a model in storage. If query is number or sting, then its will be id
+   * and find model with this id. If query is object, find appropriate models
+   *
+   * @param {string|number|object} [query] A query to match models against
+   * @param {function} [callback] The callback to call after the model(s) is(are) found
+   *
+   * @example
+   * model.find(1, function(){}); // Will find the model with an ID of 1
+   * model.find({ foo: 'bar' });
+   */
+  find(query, callback) {
 
-		let queryType = typeof query;
+    let queryType = typeof query;
     callback = callback || function () {};
-		query = query || {};
+    query = query || {};
 
-		// If query is callback, findAll and fire callback
-		if (queryType === 'function') {
-			callback = query;
-			this.storage.findAll(callback);
-		} else if (queryType === 'string' || queryType === 'number') {
-			query = parseInt(query, 10);
-			this.storage.find({ id: query }, callback);
-		} else {
-			this.storage.find(query, callback);
-		}
-	};
+    // If query is callback, findAll and fire callback
+    if (queryType === 'function') {
+      callback = query;
+      this.storage.findAll(callback);
+    } else if (queryType === 'string' || queryType === 'number') {
+      query = parseInt(query, 10);
+      this.storage.find({ id: query }, callback);
+    } else {
+      this.storage.find(query, callback);
+    }
+  };
 
-	/**
-	 * Find all models
-	 */
-	findAll(callback) {
+  /**
+   * Find all models
+   */
+  findAll(callback) {
     callback = callback || function () {};
-		this.storage.findAll(callback);
-	}
+    this.storage.findAll(callback);
+  }
 
-	/**
-	 * Updates a model by ID
-	 */
-	update(data, callback) {
+  /**
+   * Updates a model by ID
+   */
+  update(data, callback) {
     callback = callback || function () {};
 
     if (this.validate(data)) {
       let fields = this.getData(data);
       this.storage.save(fields, callback, data.id);
     }
-	}
+  }
 
-	like (id, vote, callback) {
+  like (id, vote, callback) {
     callback = callback || function () {};
     this.storage.like(id, !!vote, callback);
   }
 
-	/**
-	 * Removes a model from storage
-	 */
-	remove(id, callback) {
+  /**
+   * Removes a model from storage
+   */
+  remove(id, callback) {
     callback = callback || function () {};
-		this.storage.remove(id, callback);
-	}
+    this.storage.remove(id, callback);
+  }
 
-	/**
-	 * WARNING: Remove all data from storage.
-	 */
-	removeAll(callback) {
-    callback = callback || function () {};	
-		this.storage.drop(callback);
-	}
+  /**
+   * WARNING: Remove all data from storage.
+   */
+  removeAll(callback) {
+    callback = callback || function () {};  
+    this.storage.drop(callback);
+  }
 
   getData(data) {
-  	let fields = {}
+    let fields = {}
     this.fieldMap.forEach( (name) => {
       if (typeof data[name] !== 'undefined') {
         fields[name] = data[name];
